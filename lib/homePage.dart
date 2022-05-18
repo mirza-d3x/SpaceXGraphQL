4618e615e0463e84016e46d33662d76cc9a3bdca
mirsa <mohammedmirsa010@gmail.com>
2022-05-18 07:12:31 +0530

    Graphql Query added and used listview builder for show data

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("Graph QL Demo"),
+      ),
+      body: Query(
+        options: QueryOptions(
+          document: gql(readRepositories),
+        ),
+        builder: (QueryResult result,
+            {VoidCallback? refetch, FetchMore? fetchMore}) {
+          if (result.hasException) {
+            return Text(result.exception.toString());
+          }
+
+          if (result.isLoading) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return ListView.builder(
+              itemCount: result.data!["artworkAttributionClasses"].length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(
+                      result.data!["mission_name"][index]["rocket"][index]["rocket_name"]),
+                );
+              });
+        },
+      ),
+    );
   }
 }
+String readRepositories = r"""
+ {
+  launchesPast(limit: 5) {
+    mission_name
+    rocket {
+      rocket_name
+      rocket_type
+    }
+  }
+}
+
+""";
